6d6058618810395ddcffa88743a9e47b7d46c796
Oliver Knight <oliver.knight@charite.de>
2024-03-12 12:13:57 +0100

    Updated

--- reference_genome/human.sh
+++ reference_genome/human.sh
@@ -1,79 +1,101 @@
 #!/bin/bash
 
-# Check if a conda env to create the reference exists
-if [ ! -d "$HOME/work/bin/miniconda3/envs/genome_processing/" ]; then
-    conda create -y -n genome_processing bcftools samtools bedtools bwa
+echo ""
+echo "Setting up bedtools env"
+echo ""
+
+# Conda environment setup
+conda create -y -p "$TMPDIR/reference" bedtools || { echo "Error creating conda environment; is miniconda in your path?"; exit 1; }
+
+echo ""
+
+# Check cellranger-arc path
+cellranger_arc_path="$HOME/group/work/bin/cellranger-arc-2.0.2"
+[ ! -d "$cellranger_arc_path" ] || { echo "Error: cellranger-arc path not found"; exit 1; }
+export PATH="$cellranger_arc_path:$PATH"
+
+echo "Do you want to create the reference genome here ($PWD)? (y/newdir/n)"
+read -r choice
+
+# Validate user input
+while [[ ! $choice =~ ^[YyNn]$ && $choice != "newdir" ]]; do
+    echo "Invalid input. Please enter Y, N, or newdir"
+    read -r choice
+done
+
+if [[ $choice == "newdir" ]]; then
+    echo ""
+    echo "Enter the directory:"
+    read -r ref_directory
+
+    if [ ! -d "$ref_directory" ]; then
+        echo "Error: Directory not found, please create it"
+        exit 1
+    fi
 fi
 
-if [ ! -d "$HOME/work/bin/miniconda3/envs/genome_processing/" ]; then
-    echo -e "Error: conda env 'genome_processing' still does not exist"
+if [[ $choice =~ ^[Yy]$ ]]; then
+    echo ""
+    echo "Downloading reference files, this might take a while..."
+    echo ""
+    # Add commands for downloading reference files
+elif [[ $choice =~ ^[Nn]$ ]]; then
+    echo ""
+    echo "Not downloading reference files, exiting"
+    echo ""
+    exit
 fi
 
-conda activate genome_processing
+cd "$ref_directory" || { echo "Error: Could not change to the specified directory"; exit 1; }
 
-if [ ! -d "$HOME/group/work/bin/cellranger-arc-2.0.2" ]; then
-    echo -e "Please make sure cellranger-arc is properly pathed"
-    exit 1
-fi
-
-export PATH=$HOME/group/work/bin/cellranger-arc-2.0.2:$PATH
-
-genome="GRCh38-hardmasked-optimised-arc" # enter end folder name here
+# Genome setup
+genome="GRCh38-hardmasked-optimised-arc"
 build="${genome}-build"
-mkdir -p "${build}"
+source="${genome}-source"
+mkdir -p "${build}" "${source}"
 
-source=${genome}-source
-mkdir -p ${source}
+# Download files
+download_file() {
+    local url=$1
+    local file=$2
+    [ -f "$file" ] || curl -sS "$url" > "$file"
+}
+
 fasta_name="Homo_sapiens.GRCh38.dna_sm.primary_assembly"
-fasta_url="https://ftp.ensembl.org/pub/release-110/fasta/homo_sapiens/dna/${fasta_name}.fa.gz"
-fasta_in="${source}/${fasta_name}.fa"
-gtf_url="https://storage.googleapis.com/generecovery/human_GRCh38_optimized_annotation_v2.gtf.gz"
-gtf_in="${source}/human_GRCh38_optimized_annotation_v2.gtf"
-motifs_url="https://testjaspar.uio.no/download/data/2024/CORE/JASPAR2024_CORE_non-redundant_pfms_jaspar.txt"
-motifs_in="${source}/JASPAR2024_CORE_non-redundant_pfms_jaspar.txt"
+download_file "https://ftp.ensembl.org/pub/release-110/fasta/homo_sapiens/dna/${fasta_name}.fa.gz" "${source}/${fasta_name}.fa"
+download_file "https://storage.googleapis.com/generecovery/human_GRCh38_optimized_annotation_v2.gtf.gz" "${source}/human_GRCh38_optimized_annotation_v2.gtf"
+download_file "https://testjaspar.uio.no/download/data/2024/CORE/JASPAR2024_CORE_non-redundant_pfms_jaspar.txt" "${source}/JASPAR2024_CORE_non-redundant_pfms_jaspar.txt"
 
-if [ ! -f "${fasta_in}" ]; then
-    curl -sS "${fasta_url}" | zcat > "${fasta_in}"
-fi
-
-if [ ! -f "${gtf_in}" ]; then
-    curl -sS "${gtf_url}" | zcat > "${gtf_in}"
-fi
-
-if [ ! -f "${motifs_in}" ]; then
-    curl -sS "${motifs_url}" > "${motifs_in}"
-fi
-
-fasta_mod="${build}/$(basename "${fasta_in}").mod"
-cat "${fasta_in}" \
+# Modify fasta and motifs
+fasta_mod="${build}/${fasta_name}_hardmasked.fa.mod"
+cat "${source}/${fasta_name}.fa" \
     | sed -E 's/^>(\S+).*/>\1 \1/' \
     | sed -E 's/^>([0-9]+|[XY]) />chr\1 /' \
     | sed -E 's/^>MT />chrM /' \
     > "${fasta_mod}"
 
-motifs_mod="${build}/$(basename "${motifs_in}").mod"
-awk '{
-    if ( substr($1, 1, 1) == ">" ) {
-        print ">" $2 "_" substr($1,2)
-    } else {
-        print
-    }
-}' "${motifs_in}" > "${motifs_mod}"
+motifs_mod="${build}/JASPAR2024_CORE_non-redundant_pfms_jaspar.txt.mod"
+awk '{print ">" $2 "_" substr($1,2)}' "${source}/JASPAR2024_CORE_non-redundant_pfms_jaspar.txt" > "${motifs_mod}"
 
-curl -sS https://raw.githubusercontent.com/caleblareau/mitoblacklist/master/combinedBlacklist/hg38.full.blacklist.bed > ${source}/hg38.full.blacklist.bed
-mv ${build}/${fasta_name}.fa.mod ${build}/${fasta_name}_original.fa.mod
-bedtools maskfasta -fi ${build}/${fasta_name}_original.fa.mod -bed ${source}/hg38.full.blacklist.bed -fo ${build}/${fasta_name}_hardmasked.fa.mod
+# Mask fasta
+conda run -p $TMPDIR/reference bedtools maskfasta -fi "${fasta_mod}" -bed "${source}/hg38.full.blacklist.bed" -fo "${fasta_mod}"
 
+# Genome configuration
 config_in="${build}/genome.config"
-echo """{
-    organism: \"Homo_sapiens\"
-    genome: [\""${genome}"\"]
-    input_fasta: [\""${build}/${fasta_name}_hardmasked.fa.mod"\"]
-    input_gtf: [\""${gtf_in}"\"]
-    input_motifs: \""${motifs_mod}"\"
-    non_nuclear_contigs: [\"chrM\"]
-}""" > "${config_in}"
+cat > "${config_in}" <<EOF
+{
+    organism: "Homo_sapiens",
+    genome: ["${genome}"],
+    input_fasta: ["${fasta_mod}"],
+    input_gtf: ["${source}/human_GRCh38_optimized_annotation_v2.gtf"],
+    input_motifs: "${motifs_mod}",
+    non_nuclear_contigs: ["chrM"]
+}
+EOF
 
-cellranger-arc mkref --ref-version 'A' --config ${config_in}
+# Cellranger-arc mkref
+cellranger-arc mkref --ref-version 'A' --config "${config_in}"
 
-rm -r ${source} ${build}
+# Cleanup
+rm -r "${source}" "${build}"
+
