1508dede924b65045562140bca0fcb9f34bccb05
ollieeknight <oliver.knight@charite.de>
2024-11-16 09:58:20 +0100

    Brought into 2024

--- nextflow/bedfile.sh
+++ nextflow/bedfile.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-# Get the bed files directly from Twist online
+# Function to check if bedtools is installed
+check_bedtools() {
+    if ! command -v bedtools &> /dev/null; then
+        echo "Error: 'bedtools' is not installed or not in the PATH"
+        exit 1
+    fi
+}
 
+check_bedtools
+
+# Get the bed files directly from Twist online
 wget https://www.twistbioscience.com/sites/default/files/resources/2019-09/Twist_Exome_RefSeq_targets_hg38.bed
 wget https://www.twistbioscience.com/sites/default/files/resources/2020-04/Twist_MitoPanel_chrM_all_hg38_target.bed
 
 # Merge together and sort
-
 cat Twist* > Twist_Exome_RefSeq_MitoPanel_hg32.bed
-bedtools sort Twist_Exome_RefSeq_MitoPanel_hg32.bed > Twist_Exome_RefSeq_MitoPanel_hg32_sorted.bed
+bedtools sort -i Twist_Exome_RefSeq_MitoPanel_hg32.bed > Twist_Exome_RefSeq_MitoPanel_hg32_sorted.bed
